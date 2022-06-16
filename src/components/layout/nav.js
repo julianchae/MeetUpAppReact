0b912fe84ce1c8820050acc66f2e7c216edb8599
Julian Chae <Julianchae9@gmail.com>
2022-06-16 17:33:58 -0600

    nav bar styling

--- src/components/layout/nav.js
+++ src/components/layout/nav.js
@@ -1,11 +1,13 @@
 import {Link} from 'react-router-dom';
 
+import classes from './nav.module.css'
+
 function MainNav(){
-return <header>
-    <div>React Meet ups</div>
+return <header className={classes.header}>
+    <div className={classes.logo}>React Meet ups</div>
 
     <nav>
-        <ul>
+        <ul >
             <li> <Link to='/'>All Meet Ups</Link> </li>
             <li> <Link to='/new-meetup'>New Meetup</Link> </li>
             <li> <Link to='/favorites'>Favorites</Link> </li>
--- src/pages/AllMeetUps.js
+++ src/pages/AllMeetUps.js
@@ -1,4 +1,32 @@
+const DUMMY_DATA = [
+    {
+      id: 'm1',
+      title: 'This is a first meetup',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Meetupstreet 5, 12345 Meetup City',
+      description:
+        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+    {
+      id: 'm2',
+      title: 'This is a second meetup',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Meetupstreet 5, 12345 Meetup City',
+      description:
+        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+  ];
+
 function AllMeetUpsPage(){
-return <div>All Meet Ups page</div>;
+    
+return <section>
+<div>All Meet Ups page</div>
+{DUMMY_DATA.map((meetup)=>{
+return<li>{meetup.title}</li>
+})}
+</section>
+
 }
 export default AllMeetUpsPage;
